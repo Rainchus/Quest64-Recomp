c4dbc48bc12d4fd4665c4b446340a12dc652bd08
Sonic Dreamcaster <alejandro.asenjo88@gmail.com>
2024-08-26 21:13:55 -0300

    save hack on Title_80187ABC

--- patches/Title_80187ABC.c
+++ patches/Title_80187ABC.c
@@ -1,13 +1,26 @@
-// Controller detection bypass
 #define RECOMP_EXPORT __attribute__((section(".recomp_export")))
 #define RECOMP_PATCH __attribute__((section(".recomp_patch")))
 #define RECOMP_FORCE_PATCH __attribute__((section(".recomp_force_patch")))
 
-extern unsigned char gControllerPlugged[4];
+#include "../lib/sf64decomp/include/sf64save.h"
 
+extern unsigned char gControllerPlugged[4];
+extern SaveFile gSaveFile;
+
+// Controller detection bypass
 RECOMP_PATCH int Title_80187ABC(void) {
     int ret = -1;
     int i;
+
+    // save hack
+    for (i = 0; i < 16; i++) {
+        gSaveFile.save.data.planet[i].expertClear = 1;
+        gSaveFile.save.data.planet[i].normalClear = 1;
+        gSaveFile.save.data.planet[i].played = 1;
+        gSaveFile.save.data.planet[i].normalMedal = 1;
+        gSaveFile.save.data.planet[i].expertMedal = 1;
+    }
+    Save_Write();
 
     gControllerPlugged[0] = 1;
 
