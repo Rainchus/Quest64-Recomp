2f34f414011a9c5269380c8fd36aae34e0724cc3
Sonic Dreamcaster <alejandro.asenjo88@gmail.com>
2024-08-31 18:01:35 -0300

    authored-by: @theboy181 : dpad_control.c

---
+++ patches/dpad_control.c
@@ -0,0 +1,192 @@
+#define RECOMP_PATCH __attribute__((section(".recomp_patch")))
+#define __sinf __sinf_recomp
+#define __cosf __cosf_recomp
+
+
+#if 1
+
+#include "patches.h"
+
+extern s32 D_80161810[10];
+
+typedef struct {
+    s32 unk_0;
+    s32 unk_4;
+} UnkStruct_D_menu_801B9250;
+
+RECOMP_PATCH bool Option_8019C418(s32* arg0, s32 arg1, bool arg2, s32 arg3, s32 arg4, s32 arg5, s32 arg6, s32 arg7, UnkStruct_D_menu_801B9250* stick) {
+    s32 axis;
+    s32 x;
+    s32 y;
+    s32 temp;
+    bool ret = false;
+
+    if (arg1 == 0) {
+        return ret;
+    }
+
+    temp = *arg0;
+
+    x = gControllerPress[arg7].stick_x;
+    y = -gControllerPress[arg7].stick_y;
+
+    if (arg2 == true) {
+        axis = y;
+        if ((x > 40) || (x < -40)) {
+            return ret;
+        }
+    } else {
+        axis = x;
+        if ((y > 40) || (y < -40)) {
+            return ret;
+        }
+    }
+
+    if ((axis < arg4) && (axis > -arg4)) {
+        axis = 0;
+    }
+
+    if (!(stick->unk_4)) {
+        if (axis != 0) {
+            if (axis > 0) {
+                (*arg0)++;
+                if (*arg0 > arg1) {
+                    if (arg3 == 0) {
+                        *arg0 = 0;
+                    } else {
+                        *arg0 = arg1;
+                    }
+                }
+            }
+
+            if (axis < 0) {
+                (*arg0)--;
+                if (*arg0 < 0) {
+                    if (arg3 == 0) {
+                        *arg0 = arg1;
+                    } else {
+                        *arg0 = 0;
+                    }
+                }
+            }
+
+            stick->unk_4 = arg5 + stick->unk_0;
+            if (stick->unk_0 > 0) {
+                stick->unk_0 -= arg6;
+            }
+        } else {
+            stick->unk_4 = 0;
+            stick->unk_0 = arg6;
+        }
+    }
+
+    if (((gControllerPress[0].button & D_JPAD) || (gControllerPress[0].button & U_JPAD)) && (arg2 == true)) {
+        if (gControllerPress[0].button & D_JPAD) {
+            (*arg0)++;
+            if (*arg0 > arg1) {
+                if (arg3 == 0) {
+                    *arg0 = 0;
+                } else {
+                    *arg0 = arg1;
+                }
+            }
+        }
+
+        if (gControllerPress[0].button & U_JPAD) {
+            (*arg0)--;
+            if (*arg0 < 0) {
+                if (arg3 == 0) {
+                    *arg0 = arg1;
+                } else {
+                    *arg0 = 0;
+                }
+            }
+        }
+
+        stick->unk_4 = arg5 + stick->unk_0;
+        if (stick->unk_0 > 0) {
+            stick->unk_0 -= arg6;
+        }
+    } else if (((gControllerPress[0].button & L_JPAD) || (gControllerPress[0].button & R_JPAD)) && (arg2 == false)) {
+        if (gControllerPress[0].button & L_JPAD) {
+            (*arg0)++;
+            if (*arg0 > arg1) {
+                if (arg3 == 0) {
+                    *arg0 = 0;
+                } else {
+                    *arg0 = arg1;
+                }
+            }
+        }
+
+        if (gControllerPress[0].button & R_JPAD) {
+            (*arg0)--;
+            if (*arg0 < 0) {
+                if (arg3 == 0) {
+                    *arg0 = arg1;
+                } else {
+                    *arg0 = 0;
+                }
+            }
+        }
+
+        stick->unk_4 = arg5 + stick->unk_0;
+        if (stick->unk_0 > 0) {
+            stick->unk_0 -= arg6;
+        }
+    }
+
+    if (stick->unk_4 > 0) {
+        stick->unk_4--;
+    }
+
+    if (temp != *arg0) {
+        ret = true;
+    }
+
+    return ret;
+}
+
+RECOMP_PATCH s32 func_hud_800886B8(void) {
+    s32 var_v1 = 0;
+    f32 y = gInputPress->stick_y;
+
+    if ((y != 0.0f) && (D_80161810[4] != 0)) {
+        return 0;
+    }
+
+    D_80161810[4] = 0;
+
+    if (fabsf(y) < 30.0f) {
+        y = 0.0f;
+    }
+
+    if (y != 0.0f) {
+        if (D_80161810[2] == 0) {
+            if (y > 0) {
+                var_v1 = 1;
+            } else {
+                var_v1 = -1;
+            }
+            D_80161810[2] = 1;
+        } else {
+            D_80161810[2] = 0;
+        }
+    } else {
+        if ((gControllerPress[0].button & D_JPAD) || (gControllerPress[0].button & U_JPAD)) {
+            if (D_80161810[2] == 0) {
+                if (gControllerPress[0].button & U_JPAD) {
+                    var_v1 = 1;
+                } else {
+                    var_v1 = -1;
+                }
+                D_80161810[2] = 1;
+            }
+        } else {
+            D_80161810[2] = 0;
+        }
+    }
+    return var_v1;
+}
+
+#endif
