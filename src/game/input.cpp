f105a3de75d00ae9e24e16a204176466acc3e412
Sonic Dreamcaster <alejandro.asenjo88@gmail.com>
2024-09-10 21:54:13 -0300

    no quit prompt

--- src/game/input.cpp
+++ src/game/input.cpp
@@ -160,7 +160,8 @@ bool sdl_event_filter(void* userdata, SDL_Event* event) {
             recompui::set_current_menu(recompui::Menu::Config);
         }
 
-        zelda64::open_quit_game_prompt();
+        // zelda64::open_quit_game_prompt();
+        ultramodern::quit(); // @Starfox64Recomp
         recompui::activate_mouse();
         break;
     }
