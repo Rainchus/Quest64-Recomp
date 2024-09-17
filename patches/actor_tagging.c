c8f4105e9ba951e931c75a7fe69c06d3802343d9
Sonic Dreamcaster <alejandro.asenjo88@gmail.com>
2024-09-17 06:10:39 -0300

    Tag Arwing Character Heads

---
+++ patches/actor_tagging.c
@@ -0,0 +1,96 @@
+#include "patches.h"
+
+#define TAG_FACE (0x100)
+
+extern Limb* D_arwing_3016610[19];
+extern Animation D_arwing_3015AF4;
+extern Animation D_arwing_3015C28;
+extern s32 D_display_800CA22C;
+extern Gfx* sExpertFaceDL[4];
+extern Gfx* sFaceDL[4];
+extern Gfx D_arwing_30194E0[];
+extern Gfx D_arwing_30183D0[];
+
+bool Display_ArwingWingsOverrideLimbDraw(s32 limbIndex, Gfx** gfxPtr, Vec3f* pos, Vec3f* rot, void* wingData);
+
+RECOMP_PATCH void Display_ArwingWings(ArwingInfo* arwing) {
+    Vec3f frameTable[30];
+    s32 drawFace;
+
+    Matrix_Push(&gGfxMatrix);
+
+    arwing->laserGunsXpos = 0.0f;
+    if (arwing->laserGunsYpos < -7.0f) {
+        arwing->laserGunsXpos = (-arwing->laserGunsYpos - 7.0f) * 2.5f;
+    }
+
+    if (gGameState == GSTATE_PLAY) {
+        Animation_DrawSkeleton(1, D_arwing_3016610, gPlayer[0].jointTable, Display_ArwingWingsOverrideLimbDraw, NULL,
+                               arwing, &gIdentityMatrix);
+    } else {
+        if (gGameState == GSTATE_MENU) {
+            Animation_GetFrameData(&D_arwing_3015AF4, 0, frameTable);
+        } else {
+            Animation_GetFrameData(&D_arwing_3015C28, 0, frameTable);
+        }
+        Animation_DrawSkeleton(1, D_arwing_3016610, frameTable, Display_ArwingWingsOverrideLimbDraw, NULL, arwing,
+                               &gIdentityMatrix);
+    }
+
+    D_display_800CA22C = false;
+
+    drawFace = arwing->drawFace;
+    if (D_display_800CA220 != 0) {
+        drawFace = true;
+    }
+
+    // @recomp Tag the transform.
+    gEXMatrixGroupDecomposedNormal(gMasterDisp++, TAG_FACE + drawFace, G_EX_PUSH, G_MTX_MODELVIEW, G_EX_EDIT_ALLOW);
+
+    if (drawFace != 0) {
+        Matrix_Push(&gGfxMatrix);
+        Matrix_Translate(gGfxMatrix, 0.0f, 6.4f, -16.5f, MTXF_APPLY);
+        Matrix_RotateY(gGfxMatrix, arwing->teamFaceYrot * M_DTOR, MTXF_APPLY);
+        Matrix_RotateX(gGfxMatrix, arwing->teamFaceXrot * M_DTOR, MTXF_APPLY);
+        Matrix_Scale(gGfxMatrix, 1.0f / 70.925f, 1.0f / 70.925f, 1.0f / 70.925f, MTXF_APPLY);
+
+        if (gGameState == GSTATE_ENDING) {
+            Matrix_Scale(gGfxMatrix, 0.95f, 0.95f, 0.95f, MTXF_APPLY);
+        }
+
+        Matrix_SetGfxMtx(&gMasterDisp);
+
+        if (gExpertMode) {
+            gSPDisplayList(gMasterDisp++, sExpertFaceDL[drawFace - 1]);
+        } else {
+            gSPDisplayList(gMasterDisp++, sFaceDL[drawFace - 1]);
+        }
+        Matrix_Pop(&gGfxMatrix);
+    }
+
+    Matrix_Translate(gGfxMatrix, 0.0f, 17.2f, -25.8f, MTXF_APPLY);
+    Matrix_RotateX(gGfxMatrix, arwing->cockpitGlassXrot * M_DTOR, MTXF_APPLY);
+    Matrix_SetGfxMtx(&gMasterDisp);
+    RCP_SetupDL_64_2();
+
+    if ((gGameState == GSTATE_PLAY) && (gPlayer[0].state_1C8 == PLAYERSTATE_1C8_LEVEL_INTRO) &&
+        (gCurrentLevel == LEVEL_CORNERIA)) {
+        gDPSetPrimColor(gMasterDisp++, 0x00, 0x00, 255, 255, 255, 120);
+        gSPClearGeometryMode(gMasterDisp++, G_CULL_BACK);
+        gSPDisplayList(gMasterDisp++, D_arwing_30194E0);
+        RCP_SetupDL_46();
+        gDPSetPrimColor(gMasterDisp++, 0x00, 0x00, 255, 255, 255, 100);
+        gSPDisplayList(gMasterDisp++, D_arwing_30183D0);
+    } else {
+        RCP_SetupDL_46();
+        gDPSetPrimColor(gMasterDisp++, 0x00, 0x00, 255, 255, 255, 140);
+        gSPClearGeometryMode(gMasterDisp++, G_CULL_BACK);
+        gSPDisplayList(gMasterDisp++, D_arwing_30194E0);
+    }
+
+    gSPSetGeometryMode(gMasterDisp++, G_CULL_BACK);
+    Matrix_Pop(&gGfxMatrix);
+
+    // @recomp Pop the transform id.
+    gEXPopMatrixGroup(gMasterDisp++, G_MTX_MODELVIEW);
+}
