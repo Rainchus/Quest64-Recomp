f915cdef4b0596201912fa3cd231c59560447b34
Sonic Dreamcaster <alejandro.asenjo88@gmail.com>
2024-09-18 21:58:01 -0300

    tag the checkpoint

--- patches/item_tagging.c
+++ patches/item_tagging.c
@@ -7,7 +7,46 @@ extern Gfx D_10231A0[];
 extern Gfx D_1022E80[];
 extern Gfx D_1019820[];
 extern Gfx D_101A8E0[];
+extern Gfx D_1023C80[];
+extern Gfx D_101CAE0[];
 extern Gfx D_blue_marine_3005980[];
+
+RECOMP_PATCH void ItemCheckpoint_Draw(ItemCheckpoint* this) {
+    s32 i;
+
+    if (((gGameFrameCount & 0x18) != 0) && (this->state == 0)) {
+        Matrix_Push(&gGfxMatrix);
+        RCP_SetupDL(&gMasterDisp, SETUPDL_64);
+        gDPSetPrimColor(gMasterDisp++, 0x00, 0x00, 255, 255, 0, 255);
+        Matrix_Scale(gGfxMatrix, 3.2f, 3.2f, 3.2f, MTXF_APPLY);
+        Matrix_SetGfxMtx(&gMasterDisp);
+        gDPSetTextureFilter(gMasterDisp++, G_TF_POINT);
+        gSPDisplayList(gMasterDisp++, D_1023C80);
+        gDPSetTextureFilter(gMasterDisp++, G_TF_BILERP);
+        Matrix_Pop(&gGfxMatrix);
+    }
+
+    RCP_SetupDL(&gMasterDisp, SETUPDL_29);
+    gSPTexture(gMasterDisp++, 2000, 2000, 0, G_TX_RENDERTILE, G_ON);
+    gSPSetGeometryMode(gMasterDisp++, G_TEXTURE_GEN);
+    Matrix_RotateZ(gGfxMatrix, this->unk_58 * M_DTOR, MTXF_APPLY);
+
+    // @recomp Tag the transform.
+    gEXMatrixGroupDecomposedNormal(gMasterDisp++, TAG_ITEM + this->index, G_EX_PUSH, G_MTX_MODELVIEW,
+    G_EX_EDIT_ALLOW);
+    for (i = 0; i < 8; i++) {
+        Matrix_Push(&gGfxMatrix);
+        Matrix_RotateZ(gGfxMatrix, i * 45.0f * M_DTOR, MTXF_APPLY);
+        Matrix_Translate(gGfxMatrix, 2.0f * this->width, 0.0f, 0.0f, MTXF_APPLY);
+        Matrix_RotateZ(gGfxMatrix, (gGameFrameCount + (i * 110.0f)) * M_DTOR * 7.2f * this->unk_54, MTXF_APPLY);
+        Graphics_SetScaleMtx(2.0f * this->unk_50);
+        gSPDisplayList(gMasterDisp++, D_101CAE0);
+        Matrix_Pop(&gGfxMatrix);
+    }
+    // @recomp Pop the transform id.
+    gEXPopMatrixGroup(gMasterDisp++, G_MTX_MODELVIEW);
+    gSPClearGeometryMode(gMasterDisp++, G_TEXTURE_GEN);
+}
 
 RECOMP_PATCH void ItemSilverRing_Draw(ItemSilverRing* this) {
     RCP_SetupDL(&gMasterDisp, SETUPDL_29);
