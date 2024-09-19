e14e66f22d8c1fd7bb40f58dccf3c6a8aed7cf10
Sonic Dreamcaster <alejandro.asenjo88@gmail.com>
2024-09-19 06:09:42 -0300

    Effect tagging

--- patches/effect_tagging.c
+++ patches/effect_tagging.c
@@ -1,6 +1,11 @@
 #include "patches.h"
 
 extern Gfx aOrbDL[];
+extern Gfx D_102A010[];
+extern Gfx D_BG_PLANET_20031B0[];
+extern Gfx D_ME_6023810[];
+extern Gfx D_ME_60263F0[];
+extern Gfx D_102F5E0[];
 
 RECOMP_PATCH void Katina_LaserEnergyParticlesDraw(Effect358* this) {
     RCP_SetupDL(&gMasterDisp, SETUPDL_67);
@@ -10,10 +15,80 @@ RECOMP_PATCH void Katina_LaserEnergyParticlesDraw(Effect358* this) {
 
     Matrix_Scale(gGfxMatrix, 0.4f, 0.4f, 0.4f, MTXF_APPLY);
     Matrix_SetGfxMtx(&gMasterDisp);
-   // @recomp Tag the transform.
-    gEXMatrixGroupDecomposedNormal(gMasterDisp++, TAG_EFFECT + this->index, G_EX_PUSH, G_MTX_MODELVIEW, G_EX_EDIT_ALLOW);
+    // @recomp Tag the transform.
+    gEXMatrixGroupDecomposedNormal(gMasterDisp++, TAG_EFFECT(this), G_EX_PUSH, G_MTX_MODELVIEW, G_EX_EDIT_ALLOW);
     gSPDisplayList(gMasterDisp++, aOrbDL);
     // @recomp Pop the transform id.
     gEXPopMatrixGroup(gMasterDisp++, G_MTX_MODELVIEW);
     RCP_SetupDL(&gMasterDisp, SETUPDL_64);
+}
+
+RECOMP_PATCH void Effect_Effect343_Draw(Effect343* this) {
+    // @recomp Tag the transform.
+    gEXMatrixGroupDecomposedNormal(gMasterDisp++, TAG_EFFECT(this), G_EX_PUSH, G_MTX_MODELVIEW, G_EX_EDIT_ALLOW);
+    Graphics_SetScaleMtx(this->scale2);
+    if (this->unk_44 == 0) {
+        gDPSetPrimColor(gMasterDisp++, 0, 0, 0, 0, 0, this->unk_4A);
+        gSPDisplayList(gMasterDisp++, D_102A010);
+    } else {
+        Matrix_Scale(gGfxMatrix, 1.5f, 1.5f, 1.5f, MTXF_APPLY);
+        Matrix_SetGfxMtx(&gMasterDisp);
+        gDPSetPrimColor(gMasterDisp++, 0, 0, 255, 255, 255, this->unk_4A);
+        gSPDisplayList(gMasterDisp++, D_BG_PLANET_20031B0);
+    }
+    // @recomp Pop the transform id.
+    gEXPopMatrixGroup(gMasterDisp++, G_MTX_MODELVIEW);
+}
+
+RECOMP_PATCH void Meteo_Effect370_Draw(Effect370* this) {
+    if (this->state == 1) {
+        // @recomp Tag the transform.
+        gEXMatrixGroupDecomposedNormal(gMasterDisp++, TAG_EFFECT(this), G_EX_PUSH, G_MTX_MODELVIEW, G_EX_EDIT_ALLOW);
+        Matrix_RotateX(gGfxMatrix, this->obj.rot.x * M_DTOR, MTXF_APPLY);
+        Matrix_Scale(gGfxMatrix, this->scale2, this->scale2, this->scale2, MTXF_APPLY);
+        Matrix_RotateZ(gGfxMatrix, M_PI / 2, MTXF_APPLY);
+        Matrix_SetGfxMtx(&gMasterDisp);
+        gDPSetPrimColor(gMasterDisp++, 0, 0, 255, 255, 255, this->unk_44);
+        gSPDisplayList(gMasterDisp++, D_ME_60263F0);
+        // @recomp Pop the transform id.
+        gEXPopMatrixGroup(gMasterDisp++, G_MTX_MODELVIEW);
+    }
+}
+
+RECOMP_PATCH void Meteo_Effect369_Draw(Effect369* this) {
+    if (this->state != 0) {
+        // @recomp Tag the transform.
+        gEXMatrixGroupDecomposedNormal(gMasterDisp++, TAG_EFFECT(this), G_EX_PUSH, G_MTX_MODELVIEW, G_EX_EDIT_ALLOW);
+        Matrix_RotateX(gGfxMatrix, this->obj.rot.x * M_DTOR, MTXF_APPLY);
+        Matrix_Scale(gGfxMatrix, this->scale2, this->scale2, this->scale2 * 3.0f, MTXF_APPLY);
+
+        if ((gGameFrameCount % 2) != 0) {
+            Matrix_RotateZ(gGfxMatrix, M_PI, MTXF_APPLY);
+        }
+
+        Matrix_RotateX(gGfxMatrix, -M_PI / 2, MTXF_APPLY);
+        Matrix_SetGfxMtx(&gMasterDisp);
+        gDPSetPrimColor(gMasterDisp++, 0, 0, 160, 255, 160, this->unk_44);
+        gSPDisplayList(gMasterDisp++, D_102F5E0);
+        // @recomp Pop the transform id.
+        gEXPopMatrixGroup(gMasterDisp++, G_MTX_MODELVIEW);
+    }
+}
+
+RECOMP_PATCH void Meteo_Effect371_Draw(Effect371* this) {
+    // @recomp Tag the transform.
+    gEXMatrixGroupDecomposedNormal(gMasterDisp++, TAG_EFFECT(this), G_EX_PUSH, G_MTX_MODELVIEW, G_EX_EDIT_ALLOW);
+    Matrix_RotateX(gGfxMatrix, this->obj.rot.x * M_DTOR, MTXF_APPLY);
+    Matrix_Scale(gGfxMatrix, this->scale2, this->scale2, this->scale2, MTXF_APPLY);
+    Matrix_SetGfxMtx(&gMasterDisp);
+    gDPSetPrimColor(gMasterDisp++, 0, 0, 255, 255, 255, this->unk_44);
+
+    switch (this->unk_4C) {
+        case 0:
+            gSPDisplayList(gMasterDisp++, D_ME_6023810);
+        case 1:
+            break;
+    }
+    // @recomp Pop the transform id.
+    gEXPopMatrixGroup(gMasterDisp++, G_MTX_MODELVIEW);
 }
