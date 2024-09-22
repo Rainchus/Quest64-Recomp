d7b34ec9a33fc2b83f682b495a9711c67e7285fd
Sonic Dreamcaster <alejandro.asenjo88@gmail.com>
2024-09-22 03:28:10 -0300

    arwing tagging (WIP)

--- patches/skeleton_tagging.c
+++ patches/skeleton_tagging.c
@@ -172,9 +172,6 @@ RECOMP_PATCH void Animation_DrawLimb(s32 mode, Limb* limb, Limb** skeleton, Vec3
 
 #endif
 
-void Animation_DrawLimbOriginal(s32 mode, Limb* limb, Limb** skeleton, Vec3f* jointTable, OverrideLimbDraw overrideLimbDraw,
-                        PostLimbDraw postLimbDraw, void* data);
-
 void Animation_DrawSkeletonOriginal(s32 mode, Limb** skeletonSegment, Vec3f* jointTable, OverrideLimbDraw overrideLimbDraw,
                             PostLimbDraw postLimbDraw, void* data, Matrix* transform) {
     bool override;
@@ -292,6 +289,164 @@ void Animation_DrawLimbOriginal(s32 mode, Limb* limb, Limb** skeleton, Vec3f* jo
     if (limb->sibling != NULL) {
         Animation_DrawLimbOriginal(mode, limb->sibling, skeleton, jointTable, overrideLimbDraw, postLimbDraw, data);
     }
+}
+void Animation_DrawSkeletonArwing(s32 mode, Limb** skeletonSegment, Vec3f* jointTable, OverrideLimbDraw overrideLimbDraw,
+                            PostLimbDraw postLimbDraw, void* data, Matrix* transform) {
+    bool override;
+    Limb** skeleton;
+    Limb* rootLimb;
+    s32 rootIndex;
+    Gfx* dList;
+    Vec3f baseTrans;
+    Vec3f baseRot;
+    s32 overrideLimbIndex;
+    s32 postLimbIndex;
+    s32 tagOverride;
+    s32 tagPost;
+    ArwingInfo* actor = (ArwingInfo*) data;
+
+    Matrix_Push(&gCalcMatrix);
+    Matrix_Copy(gCalcMatrix, transform);
+
+    skeleton = SEGMENTED_TO_VIRTUAL(skeletonSegment);
+    rootLimb = SEGMENTED_TO_VIRTUAL(skeleton[0]);
+    rootIndex = Animation_GetLimbIndex(skeleton[0], skeleton);
+    baseRot = jointTable[rootIndex];
+
+    // overrideLimbIndex = rootIndex - 1;
+    overrideLimbIndex = (rootIndex - 1) != 0 ? rootIndex - 1 : 0;
+    postLimbIndex = (rootIndex - 1) != 0 ? rootIndex - 1 : 0;
+
+    if (mode & 1) {
+        baseTrans.x = rootLimb->trans.x;
+        baseTrans.y = rootLimb->trans.y;
+        baseTrans.z = rootLimb->trans.z;
+    } else {
+        baseTrans.x = jointTable[0].x;
+        baseTrans.y = jointTable[0].y;
+        baseTrans.z = jointTable[0].z;
+    }
+
+    dList = rootLimb->dList;
+    Matrix_Push(&gGfxMatrix);
+
+    // @recomp Tag the transform.
+    gEXMatrixGroupDecomposedNormal(gMasterDisp++, TAG_ARWING_SKEL(actor) + rootIndex, G_EX_PUSH, G_MTX_MODELVIEW,
+                                   G_EX_EDIT_ALLOW);
+
+    if (overrideLimbDraw == NULL) {
+        override = false;
+    } else {
+        override = overrideLimbDraw(rootIndex - 1, &dList, &baseTrans, &baseRot, data);
+    }
+    if (!override) {
+        Matrix_Translate(gCalcMatrix, baseTrans.x, baseTrans.y, baseTrans.z, MTXF_APPLY);
+        Matrix_RotateZ(gCalcMatrix, baseRot.z * M_DTOR, MTXF_APPLY);
+        Matrix_RotateY(gCalcMatrix, baseRot.y * M_DTOR, MTXF_APPLY);
+        Matrix_RotateX(gCalcMatrix, baseRot.x * M_DTOR, MTXF_APPLY);
+        if (dList != NULL) {
+            Matrix_Mult(gGfxMatrix, gCalcMatrix, MTXF_APPLY);
+            Matrix_SetGfxMtx(&gMasterDisp);
+            gSPDisplayList(gMasterDisp++, dList);
+        }
+    }
+
+    if (postLimbDraw != NULL) {
+        postLimbDraw(rootIndex - 1, &baseRot, data);
+    }
+
+    // @recomp Pop the transform id.
+    gEXPopMatrixGroup(gMasterDisp++, G_MTX_MODELVIEW);
+
+    Matrix_Pop(&gGfxMatrix);
+
+    if (rootLimb->child != NULL) {
+        Animation_DrawLimbArwing(mode, rootLimb->child, skeleton, jointTable, overrideLimbDraw, postLimbDraw, data);
+    }
+
+    Matrix_Pop(&gCalcMatrix);
+
+    if (mode >= 2) {
+        Matrix_Mult(gGfxMatrix, gCalcMatrix, MTXF_APPLY);
+    }
+}
+
+void Animation_DrawLimbArwing(s32 mode, Limb* limb, Limb** skeleton, Vec3f* jointTable, OverrideLimbDraw overrideLimbDraw,
+                        PostLimbDraw postLimbDraw, void* data) {
+    bool override;
+    s32 limbIndex;
+    Gfx* dList;
+    Vec3f trans;
+    Vec3f rot;
+    Vec3f pos;
+    Vec3f origin = { 0.0f, 0.0f, 0.0f };
+    ArwingInfo* actor = (ArwingInfo*) data;
+    s32 childLimbIndex;
+    s32 siblingLimbIndex;
+
+    Matrix_Push(&gCalcMatrix);
+
+    limbIndex = Animation_GetLimbIndex(limb, skeleton);
+
+    // @recomp Tag the transform.
+    gEXMatrixGroupDecomposedNormal(gMasterDisp++, TAG_ARWING_SKEL(actor) + 768 + limbIndex, G_EX_PUSH, G_MTX_MODELVIEW,
+                                   G_EX_EDIT_ALLOW);
+    limb = SEGMENTED_TO_VIRTUAL(limb);
+    rot = jointTable[limbIndex];
+    trans.x = limb->trans.x;
+    trans.y = limb->trans.y;
+    trans.z = limb->trans.z;
+    dList = limb->dList;
+    Matrix_Push(&gGfxMatrix);
+
+    if (overrideLimbDraw == NULL) {
+        override = false;
+    } else {
+        override = overrideLimbDraw(limbIndex - 1, &dList, &trans, &rot, data);
+    }
+    if (!override) {
+        Matrix_Translate(gCalcMatrix, trans.x, trans.y, trans.z, MTXF_APPLY);
+        Matrix_RotateZ(gCalcMatrix, rot.z * M_DTOR, MTXF_APPLY);
+        Matrix_RotateY(gCalcMatrix, rot.y * M_DTOR, MTXF_APPLY);
+        Matrix_RotateX(gCalcMatrix, rot.x * M_DTOR, MTXF_APPLY);
+        if (dList != NULL) {
+            if (mode >= 2) {
+                Matrix_MultVec3f(gCalcMatrix, &origin, &pos);
+                if (mode != 5) {
+                    Display_SetSecondLight(&pos);
+                }
+            }
+            Matrix_Mult(gGfxMatrix, gCalcMatrix, MTXF_APPLY);
+            Matrix_SetGfxMtx(&gMasterDisp);
+            gSPDisplayList(gMasterDisp++, dList);
+        }
+    }
+    // @recomp Pop the transform id.
+    gEXPopMatrixGroup(gMasterDisp++, G_MTX_MODELVIEW);
+
+    if (postLimbDraw != NULL) {
+        postLimbDraw(limbIndex - 1, &rot, data);
+    }
+    Matrix_Pop(&gGfxMatrix);
+    if (limb->child != NULL) {
+        childLimbIndex = Animation_GetLimbIndex(limb->child, skeleton);
+        // @recomp Tag the transform.
+        gEXMatrixGroupDecomposedNormal(gMasterDisp++, TAG_ARWING_SKEL(actor) + 256 + childLimbIndex, G_EX_PUSH,
+                                       G_MTX_MODELVIEW, G_EX_EDIT_ALLOW);
+        Animation_DrawLimbArwing(mode, limb->child, skeleton, jointTable, overrideLimbDraw, postLimbDraw, data);
+    }
+    // @recomp Pop the transform id.
+    gEXPopMatrixGroup(gMasterDisp++, G_MTX_MODELVIEW);
+    Matrix_Pop(&gCalcMatrix);
+    if (limb->sibling != NULL) {
+        siblingLimbIndex = Animation_GetLimbIndex(limb->sibling, skeleton);
+        // @recomp Tag the transform.
+        gEXMatrixGroupDecomposedNormal(gMasterDisp++, TAG_ARWING_SKEL(actor) + 512 + siblingLimbIndex, G_EX_PUSH,
+                                       G_MTX_MODELVIEW, G_EX_EDIT_ALLOW);
+        Animation_DrawLimbArwing(mode, limb->sibling, skeleton, jointTable, overrideLimbDraw, postLimbDraw, data);
+    }
+    // @recomp Pop the transform id.
+    gEXPopMatrixGroup(gMasterDisp++, G_MTX_MODELVIEW);
 }
 
 RECOMP_PATCH void Andross_AndPassage_Draw(AndPassage* this) {
