e543a86ceb0f8b15ba3467ec669a3457e9ac06ed
Sonic Dreamcaster <alejandro.asenjo88@gmail.com>
2024-09-21 22:01:41 -0300

    tagging header (WIP)

--- patches/sf64_tagging.h
+++ patches/sf64_tagging.h
@@ -3,26 +3,36 @@
 
 /* For dynamic actors */
 #define TAG_ADDRESS(ptr) ((u32) (ptr) & 0x0FFFFFFF)
+#define TAG_ADDRESS_SKEL(ptr) ((u32) ((ptr) << 16) & 0x0FFF0000)
+
+/* 
+    X    XXX    XX     XX
+    TYPE  ID  INDEX  CUSTOM
+*/
+#define TAG_OBJ(ptr) (((((ptr)->obj.id) << 16) & 0x0FFF0000) | ((((ptr)->index) << 8) & 0x0000FF00))
 
 /* Actor Types */
-#define TAG_ACTOR(actor) ((u32) ((0x10000000) | (TAG_ADDRESS(actor))))
-#define TAG_ACTOR_EVENT(actor) ((u32) ((0x20000000) | (TAG_ADDRESS(actor))))
-#define TAG_BOSS(boss) ((u32) ((0x30000000) | (TAG_ADDRESS(boss))))
-#define TAG_SCENERY(scenery) ((u32) ((0x40000000) | (TAG_ADDRESS(scenery))))
-#define TAG_SCENERY_360(scenery360) ((u32) ((0x50000000) | (TAG_ADDRESS(scenery360))))
-#define TAG_EFFECT(effect) ((u32) ((0x60000000) | (TAG_ADDRESS(effect))))
-#define TAG_ITEM(item) ((u32) ((0x70000000) | (TAG_ADDRESS(item))))
+#define TAG_ACTOR(actor) ((u32) (0x10000000 | (TAG_OBJ(actor))))
+#define TAG_ACTOR_EVENT(actor) ((u32) (0x20000000 | (TAG_OBJ(actor))))
+#define TAG_BOSS(boss) ((u32) (0x30000000 | (TAG_OBJ(boss))))
+#define TAG_SCENERY(scenery) ((u32) (0x40000000 | (TAG_OBJ(scenery))))
+#define TAG_SCENERY_360(scenery360) ((u32) (0x50000000 | (TAG_ADDRESS(scenery360))))
+#define TAG_EFFECT(effect) ((u32) (0x60000000 | (TAG_OBJ(effect))))
+#define TAG_ITEM(item) ((u32) (0x70000000 | (TAG_OBJ(item))))
+#define TAG_LIMB(limb) ((u32) (0x80000000 | (TAG_ADDRESS(limb))))
+#define TAG_ACTOR_SKEL(limb) ((u32) (0x90000000 | (TAG_OBJ(limb))))
 
 /* Derivatives */
-#define TAG_ACTOR_EVENT_COMMON(actor) (TAG_ACTOR_EVENT(actor) + 0x1000)
+#define TAG_ACTOR_EVENT_COMMON(actor) (TAG_ACTOR_EVENT(actor) + 1)
 #define TAG_FACE (0x100)
 #define TAG_CS_GREAT_FOX (0x200)
 
 /* Map */
-#define TAG_PLANET_SHADOW (0x300)
+#define TAG_PLANET_SHADOW (0x90000000)
 #define TAG_PLANET_ANIM (TAG_PLANET_SHADOW + PLANET_MAX)
 #define TAG_PLANET_ANIM_2 (TAG_PLANET_ANIM + PLANET_MAX)
 #define TAG_PLANET_METEOR (TAG_PLANET_SHADOW + PLANET_MAX)
+#define TAG_PLANET_EXPLOSIONS (TAG_PLANET_METEOR + 0x100)
 
 #define ACTOR_TRANSFORM_LIMB_COUNT 256
 #define ACTOR_TRANSFORM_ID_COUNT (ACTOR_TRANSFORM_LIMB_COUNT * 2) // One ID for each limb and another for each post-draw
