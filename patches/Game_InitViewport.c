220485c4afc43452dbf61275d54a1e5f24cf4272
Sonic Dreamcaster <alejandro.asenjo88@gmail.com>
2025-08-15 13:42:56 -0300

    Setup widescreen viewports for multiplayer
    Co-authored-by: Dario <dariosamo@gmail.com>

--- patches/Game_InitViewport.c
+++ patches/Game_InitViewport.c
@@ -1,106 +1,170 @@
-#define RECOMP_PATCH __attribute__((section(".recomp_patch")))
-
-#include "../lib/sf64decomp/include/PR/ultratypes.h"
-#include "../lib/sf64decomp/include/PR/gbi.h"
-#include "../lib/sf64decomp/include/sf64thread.h"
-#include "../lib/sf64decomp/include/sys.h"
-#include "rt64_extended_gbi.h"
 #include "patches.h"
-#define SCREEN_WIDTH 320
-#define SCREEN_HEIGHT 240
-#define SCREEN_MARGIN 0
 
 void Game_InitFullViewport(void);
 
 #if 1
 
-// TODO: This function crashes the Recompiler.
-
 RECOMP_PATCH void Game_InitViewport(Gfx** dList, u8 camCount, u8 camIndex) {
-    OSContPad* cont = &gControllerPress[0];
     static s16 ulx = 0;
     static s16 lrx = 0;
+
+// test code
+#if 0
+    OSContPad* cont = &gControllerPress[0];
     static s16 vtrans = 0;
 
-    if (cont->button & L_JPAD) {
+    if (cont->button & D_JPAD) {
         ulx -= 10;
     }
-    if (cont->button & R_JPAD) {
+    if (cont->button & U_JPAD) {
         ulx += 10;
     }
-    if (cont->button & D_JPAD) {
+    if (cont->button & L_JPAD) {
         vtrans -= 10;
     }
-    if (cont->button & U_JPAD) {
+    if (cont->button & R_JPAD) {
         vtrans += 10;
     }
+#endif
 
     switch (camCount) {
         case 4:
             switch (camIndex) {
                 case 0:
-                    gViewport->vp.vscale[0] = SCREEN_WIDTH * (gVsViewScale - 1.0f) * 2;
+                    gViewport->vp.vscale[0] =
+                        (SCREEN_HEIGHT * (gVsViewScale - 1.0f) * 2) * recomp_get_target_aspect_ratio(4.0f / 3.0f);
                     gViewport->vp.vscale[1] = SCREEN_HEIGHT * (gVsViewScale - 1.0f) * 2;
                     gViewport->vp.vscale[2] = G_MAXZ / 2;
                     gViewport->vp.vscale[3] = 0;
-                    gViewport->vp.vtrans[0] = vtrans;
-                    gViewport->vp.vtrans[1] = SCREEN_HEIGHT * (2.0f - gVsViewScale) * 2;
+                    gViewport->vp.vtrans[0] =
+                        ((SCREEN_HEIGHT * (2.0f - gVsViewScale) * 2) * -recomp_get_target_aspect_ratio(4.0f / 3.0f));
+                    gViewport->vp.vtrans[1] = (SCREEN_HEIGHT * (2.0f - gVsViewScale) * 2);
                     gViewport->vp.vtrans[2] = G_MAXZ / 2;
                     gViewport->vp.vtrans[3] = 0;
+
                     ulx = (gViewport->vp.vtrans[0] - gViewport->vp.vscale[0]) / 4;
                     lrx = (gViewport->vp.vtrans[0] + gViewport->vp.vscale[0]) / 4;
-                    recomp_printf("ulx : %d lrx : %d\n", ulx, lrx);
-                    recomp_printf("vtrans : %d\n", vtrans);
-                    // gDPSetScissor((*dList)++, G_SC_NON_INTERLACE, SCREEN_MARGIN, SCREEN_MARGIN, SCREEN_WIDTH / 2,
-                    //                SCREEN_HEIGHT / 2);
+
+#if 1
+                    gEXMatrixGroup(gMasterDisp++, 0xFFFFAAAA, G_EX_INTERPOLATE_SIMPLE, G_EX_NOPUSH, G_MTX_PROJECTION,
+                                   G_EX_COMPONENT_INTERPOLATE, G_EX_COMPONENT_INTERPOLATE, G_EX_COMPONENT_SKIP,
+                                   G_EX_COMPONENT_SKIP, G_EX_COMPONENT_INTERPOLATE, G_EX_COMPONENT_INTERPOLATE,
+                                   G_EX_COMPONENT_INTERPOLATE, G_EX_ORDER_LINEAR, G_EX_EDIT_NONE, G_EX_ASPECT_ADJUST);
+
                     gEXSetScissor((*dList)++, G_SC_NON_INTERLACE, G_EX_ORIGIN_CENTER, G_EX_ORIGIN_CENTER, ulx, 0, lrx,
                                   SCREEN_HEIGHT / 2);
                     gEXViewport((*dList)++, G_EX_ORIGIN_CENTER, gViewport++);
+#else
+                    gDPSetScissor((*dList)++, G_SC_NON_INTERLACE, 0, 0, 0, 0);
+#endif
                     break;
+
                 case 1:
-                    gViewport->vp.vscale[0] = SCREEN_WIDTH * (gVsViewScale - 1.0f) * 2;
+                    gViewport->vp.vscale[0] =
+                        (SCREEN_HEIGHT * (gVsViewScale - 1.0f) * 2) * recomp_get_target_aspect_ratio(4.0f / 3.0f);
                     gViewport->vp.vscale[1] = SCREEN_HEIGHT * (gVsViewScale - 1.0f) * 2;
                     gViewport->vp.vscale[2] = G_MAXZ / 2;
                     gViewport->vp.vscale[3] = 0;
-                    gViewport->vp.vtrans[0] = SCREEN_WIDTH * gVsViewScale * 2;
+                    gViewport->vp.vtrans[0] =
+                        (SCREEN_HEIGHT * (gVsViewScale - 1.0f) * 2) * recomp_get_target_aspect_ratio(4.0f / 3.0f);
                     gViewport->vp.vtrans[1] = SCREEN_HEIGHT * (2.0f - gVsViewScale) * 2;
                     gViewport->vp.vtrans[2] = G_MAXZ / 2;
                     gViewport->vp.vtrans[3] = 0;
-                    gDPSetScissor((*dList)++, G_SC_NON_INTERLACE, 0, 0,
-                                  0, 0);
+
+                    ulx = (gViewport->vp.vtrans[0] - gViewport->vp.vscale[0]) / 4;
+                    lrx = (gViewport->vp.vtrans[0] + gViewport->vp.vscale[0]) / 4;
+
+#if 1
+                    gEXMatrixGroup(gMasterDisp++, 0xFFFFAAAB, G_EX_INTERPOLATE_SIMPLE, G_EX_NOPUSH, G_MTX_PROJECTION,
+                                   G_EX_COMPONENT_INTERPOLATE, G_EX_COMPONENT_INTERPOLATE, G_EX_COMPONENT_SKIP,
+                                   G_EX_COMPONENT_SKIP, G_EX_COMPONENT_INTERPOLATE, G_EX_COMPONENT_INTERPOLATE,
+                                   G_EX_COMPONENT_INTERPOLATE, G_EX_ORDER_LINEAR, G_EX_EDIT_NONE, G_EX_ASPECT_ADJUST);
+
+                    gEXSetScissor((*dList)++, G_SC_NON_INTERLACE, G_EX_ORIGIN_CENTER, G_EX_ORIGIN_CENTER, ulx, 0, lrx,
+                                  SCREEN_HEIGHT / 2);
+                    gEXViewport((*dList)++, G_EX_ORIGIN_CENTER, gViewport++);
+#else
+                    gDPSetScissor((*dList)++, G_SC_NON_INTERLACE, 0, 0, 0, 0);
+#endif
                     break;
+
                 case 2:
-                    gViewport->vp.vscale[0] = SCREEN_WIDTH * (gVsViewScale - 1.0f) * 2;
+                    gViewport->vp.vscale[0] =
+                        (SCREEN_HEIGHT * (gVsViewScale - 1.0f) * 2) * recomp_get_target_aspect_ratio(4.0f / 3.0f);
                     gViewport->vp.vscale[1] = SCREEN_HEIGHT * (gVsViewScale - 1.0f) * 2;
                     gViewport->vp.vscale[2] = G_MAXZ / 2;
                     gViewport->vp.vscale[3] = 0;
-                    gViewport->vp.vtrans[0] = SCREEN_WIDTH * (2.0f - gVsViewScale) * 2;
+                    gViewport->vp.vtrans[0] =
+                        ((SCREEN_HEIGHT * (2.0f - gVsViewScale) * 2) * -recomp_get_target_aspect_ratio(4.0f / 3.0f));
                     gViewport->vp.vtrans[1] = SCREEN_HEIGHT * gVsViewScale * 2;
                     gViewport->vp.vtrans[2] = G_MAXZ / 2;
                     gViewport->vp.vtrans[3] = 0;
-                    gDPSetScissor((*dList)++, G_SC_NON_INTERLACE, 0, 0,
-                                  0, 0);
+
+                    ulx = (gViewport->vp.vtrans[0] - gViewport->vp.vscale[0]) / 4;
+                    lrx = (gViewport->vp.vtrans[0] + gViewport->vp.vscale[0]) / 4;
+#if 1
+                    gEXMatrixGroup(gMasterDisp++, 0xFFFFAAAC, G_EX_INTERPOLATE_SIMPLE, G_EX_NOPUSH, G_MTX_PROJECTION,
+                                   G_EX_COMPONENT_INTERPOLATE, G_EX_COMPONENT_INTERPOLATE, G_EX_COMPONENT_SKIP,
+                                   G_EX_COMPONENT_SKIP, G_EX_COMPONENT_INTERPOLATE, G_EX_COMPONENT_INTERPOLATE,
+                                   G_EX_COMPONENT_INTERPOLATE, G_EX_ORDER_LINEAR, G_EX_EDIT_NONE, G_EX_ASPECT_ADJUST);
+
+                    gEXSetScissor((*dList)++, G_SC_NON_INTERLACE, G_EX_ORIGIN_CENTER, G_EX_ORIGIN_CENTER, ulx,
+                                  SCREEN_HEIGHT / 2, lrx, SCREEN_HEIGHT);
+                    gEXViewport((*dList)++, G_EX_ORIGIN_CENTER, gViewport++);
+
+#else
+                    gDPSetScissor((*dList)++, G_SC_NON_INTERLACE, 0, 0, 0, 0);
+#endif
                     break;
+
                 case 3:
-                    gViewport->vp.vscale[0] = SCREEN_WIDTH * (gVsViewScale - 1.0f) * 2;
+                    gViewport->vp.vscale[0] =
+                        (SCREEN_HEIGHT * (gVsViewScale - 1.0f) * 2) * recomp_get_target_aspect_ratio(4.0f / 3.0f);
                     gViewport->vp.vscale[1] = SCREEN_HEIGHT * (gVsViewScale - 1.0f) * 2;
                     gViewport->vp.vscale[2] = G_MAXZ / 2;
                     gViewport->vp.vscale[3] = 0;
-                    gViewport->vp.vtrans[0] = SCREEN_WIDTH * gVsViewScale * 2;
+                    gViewport->vp.vtrans[0] =
+                        (SCREEN_HEIGHT * (gVsViewScale - 1.0f) * 2) * recomp_get_target_aspect_ratio(4.0f / 3.0f);
                     gViewport->vp.vtrans[1] = SCREEN_HEIGHT * gVsViewScale * 2;
                     gViewport->vp.vtrans[2] = G_MAXZ / 2;
                     gViewport->vp.vtrans[3] = 0;
-                    gDPSetScissor((*dList)++, G_SC_NON_INTERLACE, 0, 0,
-                                  0, 0);
+
+                    ulx = (gViewport->vp.vtrans[0] - gViewport->vp.vscale[0]) / 4;
+                    lrx = (gViewport->vp.vtrans[0] + gViewport->vp.vscale[0]) / 4;
+
+#if 1
+                    gEXMatrixGroup(gMasterDisp++, 0xFFFFAAAC, G_EX_INTERPOLATE_SIMPLE, G_EX_NOPUSH, G_MTX_PROJECTION,
+                                   G_EX_COMPONENT_INTERPOLATE, G_EX_COMPONENT_INTERPOLATE, G_EX_COMPONENT_SKIP,
+                                   G_EX_COMPONENT_SKIP, G_EX_COMPONENT_INTERPOLATE, G_EX_COMPONENT_INTERPOLATE,
+                                   G_EX_COMPONENT_INTERPOLATE, G_EX_ORDER_LINEAR, G_EX_EDIT_NONE, G_EX_ASPECT_ADJUST);
+
+                    gEXSetScissor((*dList)++, G_SC_NON_INTERLACE, G_EX_ORIGIN_CENTER, G_EX_ORIGIN_CENTER, ulx,
+                                  SCREEN_HEIGHT / 2, lrx, SCREEN_HEIGHT);
+                    gEXViewport((*dList)++, G_EX_ORIGIN_CENTER, gViewport++);
+#else
+                    gDPSetScissor((*dList)++, G_SC_NON_INTERLACE, 0, 0, 0, 0);
+#endif
                     break;
+
                 default:
                     Game_InitFullViewport();
                     break;
             }
+
+// Test code
+#if 0
+            recomp_printf("gViewport->vp.vtrans[0] : %d\n", gViewport->vp.vtrans[0]);
+            recomp_printf("gViewport->vp.vscale[0] : %d\n", gViewport->vp.vscale[0]);
+
+            recomp_printf("ulx : %d lrx : %d uly : %d lry : %d\n", ulx, lrx, uly, lry);
+            recomp_printf("vtrans : %d\n", vtrans);
+#endif
             break;
+
         case 1:
         default:
             Game_InitFullViewport();
+            gSPViewport((*dList)++, gViewport++);
             break;
     }
     // gSPViewport((*dList)++, gViewport++);
