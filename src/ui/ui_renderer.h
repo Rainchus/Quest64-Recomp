c7b7145cdeb829c70a0f9ee086326ac45d6a011e
Sonic Dreamcaster <alejandro.asenjo88@gmail.com>
2025-12-25 04:47:43 -0300

    Revert "Update Main code to plume based RT64"
    This reverts commit 9323d9621c6604e497a739e6fbb7286da636419f.

--- src/ui/ui_renderer.h
+++ src/ui/ui_renderer.h
@@ -25,11 +25,11 @@ namespace recompui {
         RmlRenderInterface_RT64();
         ~RmlRenderInterface_RT64();
         void reset();
-        void init(plume::RenderInterface* interface, plume::RenderDevice* device);
+        void init(RT64::RenderInterface* interface, RT64::RenderDevice* device);
         Rml::RenderInterface* get_rml_interface();
         
-        void start(plume::RenderCommandList* list, int image_width, int image_height);
-        void end(plume::RenderCommandList* list, plume::RenderFramebuffer* framebuffer);
+        void start(RT64::RenderCommandList* list, int image_width, int image_height);
+        void end(RT64::RenderCommandList* list, RT64::RenderFramebuffer* framebuffer);
         void queue_image_from_bytes_file(const std::string &src, const std::vector<char> &bytes);
         void queue_image_from_bytes_rgba32(const std::string &src, const std::vector<char> &bytes, uint32_t width, uint32_t height);
     };
